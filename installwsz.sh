8cfaa69407792516ed9ecef29727b5f9fc199fab
AndyCrowd <AndyCrowd@users.noreply.github.com>
2016-04-15 11:40:51 +0200

    Update installwsz.sh

--- installwsz.sh
+++ installwsz.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
+#Version 1.0.0-1
+#
+# License: GPL3
+#
+# Author: Andy Crowd
+# Tested in Arch Linux
+
+SysPath="/usr/share/skins/WSZ"
+
 case "$1" in
-	list)
-	curl http://drweb.nu/WSZ/list.txt
-	;;
-	get)
-		if [[  ! -z "$2" ]];then
-		SkinsToGet=(${@:2});		
-		BB=($SkinsToGet)
+        list)
+        curl http://drweb.nu/WSZ/list.txt 2> /dev/null
+        ;;
+        get)
+                if [[ "$UID" != 0  ]];then echo 'You must be root!
+Skins will be downloaded into '"$SysPath";fi
+                if [[  ! -z "$2" ]];then
+                SkinsToGet=(${@:2});
+                BB=($SkinsToGet)
 echo ${#SkinsToGet[@]}
-		for I in "${SkinsToGet[@]}";do 
-		DName=$(dirname "$I")
-		if [[  ! -d "$DName" ]];then mkdir -p "$DName";fi
-			DD="http://drweb.nu/WSZ/$I"
-			((AA++))
-			echo "$I" "    $AA"
-			curl http://drweb.nu/WSZ/"$I" > $I
-		done	
-	fi
-	;;
+                for I in "${SkinsToGet[@]}";do
+                DName=$(dirname "/${I}")
+                if [[  ! -d "${SysPath}/${DName}" ]];then mkdir -p "${SysPath}/${DName}";fi
+                  curl http://drweb.nu/WSZ/"$I" > "${SysPath}/${I}"
+                done
+        fi
+        ;;
+        set | symset)
+        find "${SysPath}"  -iname "*\.wsz" -type f -print | awk '{A=$0;gsub(/[^*]*\//,"",A);system("ln -vs "$0" ./"A) }'
+        ;;
+        *)
+echo 'Warning!!! This script is not realted to AOL/Winamp!
+It is just a tool for downloading!
+
+Usage examples:
+# Downloading wsz skins with "rock" in their name
+install get $(installwsz.sh list | grep -i rock)
+# Creating symlinks to skins in the current directory
+install set.'
+        ;;
 esac
